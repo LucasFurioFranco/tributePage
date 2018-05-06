504d0e2c0dad9cf85a236a35e813b9adf31e96ee
Lucas Furio Franco <lucasfuriofranco@gmail.com>
2018-05-06 14:45:10 -0300

    primeita tentativa frustrada de nav_bar

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tributePage
 Link to the exercise page: https://www.freecodecamp.org/challenges/build-a-tribute-page
-
+Link to stage page: https://rawgit.com/LucasFurioFranco/tributePage/master/index.html
--- static/script/js.js
+++ static/script/js.js
@@ -1,5 +1,37 @@
-try{
+try {
   console.info("js.js successfuly loaded");
-} catch(ex) {
+} catch (ex) {
   console.info("js.js failed", ex);
 }
+
+window.jsLib = (function() {
+  function addEvent(target, evt, fn, passive, capture) {
+    passive = !!passive;
+    capture = !!capture;
+    if (target.addEventListener) {
+      try {
+        target.addEventListener(evt, fn, {
+          passive: passive,
+          capture: capture
+        }); // chrome 51+
+      } catch (e) {
+        if (passive || capture) {
+          console.info(e);
+        }
+        target.addEventListener(evt, fn, true);
+      }
+    } else if (target.attachEvent) {
+      target.attachEvent('on' + evt, function(evt_) {
+        fn.call(target, evt_);
+      });
+    } else if (!target['on' + evt]) {
+      target['on' + evt] = function handler(evt_) {
+        fn.call(target, evt_);
+      };
+    }
+  }
+
+  return {
+    addEvent: addEvent
+  };
+})();
